d31a451b0ba86f174cad7ab6cab49a4635f714ec
walid <walid.sa3d@gmail.com>
2015-11-05 14:41:20 +0100

    update README, format code

---
+++ README.md
@@ -0,0 +1,10 @@
+#Cpasbien
+A scraper for the french torrent site cpasbien.pw
+
+#Install
+```
+[sudo] npm install cpasbien
+```
+
+#License
+MIT
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-var rq = require('request-promise');
-var cheerio = require('cheerio');
+var rq = require('request-promise')
+var cheerio = require('cheerio')
 
-var base_url = "http://www.cpasbien.pw";
-var search = function(query, cb) {
-    var search_query = encodeURIComponent(query);
-    var search_url = base_url + "/recherche/" + search_query + ".html";
-    console.log(search_url);
-    var torrents = [];
-    var options = {
-        uri: search_url,
-        transform: function(body) {
-            return cheerio.load(body);
-        }
-    };
+const base_url = 'http://www.cpasbien.pw'
 
-    rq(search_url).then(function($) {
-        if ($("div .ligne0").length > 0) {
-            $("div[class^='ligne']").each(function(index, element) {
-                var torrent = {};
-                torrent.page = $(element).children("a").attr('href');
-                torrent.title = $(element).children("a").text();
-                torrent.size = $(element).children(".poid").text();
-                torrent.seeds = $(element).find(".seed_ok").text();
-                torrent.leech = $(element).find(".down").text();
-                torrent.date_added = undefined;
-                torrents.push(data_content);
+var search = function (query, cb) {
+  var search_query = encodeURIComponent(query)
+  var search_url = base_url + '/recherche/' + search_query + '.html'
+  var torrents = []
+  var options = {
+    uri: search_url,
+    transform: function (body) {
+      return cheerio.load(body)
+    }
+  }
 
-            });
-        }
-        cb(torrents);
+  rq(options).then(function ($) {
+    if ($('div .ligne0').length > 0) {
+      $("div[class^='ligne']").each(function (index, element) {
+        var torrent = {}
+        torrent.page = $(element).children('a').attr('href')
+        torrent.title = $(element).children('a').text()
+        torrent.size = $(element).children('.poid').text()
+        torrent.seeds = $(element).find('.seed_ok').text()
+        torrent.leech = $(element).find('.down').text()
+        torrent.date_added = undefined
+        torrents.push(torrent)
+      })
+    }
+    cb(torrents)
+  })
+}
 
-  });
-};
+module.exports.search = search
